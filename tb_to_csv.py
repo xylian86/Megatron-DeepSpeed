53181c80472921a5ab5352369580c01c4d0ebeb4
Lev Kurilenko <lekurile@microsoft.com>
2024-02-15 21:48:38 +0000

    Add matplotlib code

--- tb_to_csv.py
+++ tb_to_csv.py
@@ -1,58 +1,57 @@
+import os
 import pandas as pd
+import matplotlib.pyplot as plt
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
 parser.add_argument("--tb_dir", required=True, type=str, help="Directory for tensorboard output")
-#parser.add_argument("--tb_dir_single", required=True, type=str, help="Directory for single tensorboard output")
-#parser.add_argument("--tb_dir_full", required=True, type=str, help="Directory for many tensorboard outputs")
 args = parser.parse_args()
 
-#def find_files_recursive(directory, file_extension):
-#    """
-#    Recursively searches for files with a specific extension in a directory.
-#
-#    Args:
-#        directory (str): The path to the directory to search.
-#        file_extension (str): The desired file extension (e.g., '.txt', '.jpg').
-#
-#    Returns:
-#        list: A list of absolute paths to matching files.
-#    """
-#    matching_files = []
-#    for root, _, files in os.walk(directory):
-#        for filename in files:
-#            if filename.lower().startswith(file_extension.lower()):
-#                matching_files.append(os.path.join(root, filename))
-#    return matching_files
-#
+def find_files_recursive(directory, file_affix):
+    """
+    Recursively searches for files with a specific affix in a directory.
+
+    Args:
+        directory (str): The path to the directory to search.
+        file_affix (str): The desired file affix
+
+    Returns:
+        list: A list of paths to matching files.
+    """
+    matching_paths = []
+    for root, _, files in os.walk(directory):
+        for filename in files:
+            if filename.lower().startswith(file_affix.lower()):
+                matching_paths.append(os.path.join(root))
+    return matching_paths
+
 log_dir = args.tb_dir
-#
-#
-#
-## Example usage:
-#directory_path = './'
-#target_affix = 'events.out.tfevents'
-#result_files = find_files_recursive(directory_path, target_extension)
-#
-#for filename in result_files:
-#    #if '.pt' in filename:
-#	#print(f"LOADING CKPT FOR: {filename}")
-#	ckpt = torch.load(filename)
-#	print(f"filename: {filename}, ckpt.keys(): {ckpt.keys()}")
-#
-#print(result_files)
 
+target_affix = 'events.out.tfevents'
+tb_log_paths = find_files_recursive(log_dir, target_affix)
 
-event_accumulator = EventAccumulator(log_dir)
-event_accumulator.Reload()
+i = 0
+for tb_path in tb_log_paths:
+    print(f"tb_path: {tb_path}")
 
-events = event_accumulator.Scalars('lm-loss-training/lm loss')
+    event_accumulator = EventAccumulator(tb_path)
+    event_accumulator.Reload()
 
-x = [x.step for x in events]
-y = [x.value for x in events]
+    events = event_accumulator.Scalars('lm-loss-training/lm loss')
 
+    x = [x.step for x in events]
+    y = [x.value for x in events]
 
-df = pd.DataFrame({"step": x, "value": y})
-df.to_csv("train_loss_uc.csv")
-print(df)
+    plt.plot(x, y, label=f'UC Training Run {i}')
+
+    df = pd.DataFrame({"step": x, "value": y})
+    df.to_csv(f"file{i}.csv")
+    i = i + 1
+    print(df)
+
+plt.legend()
+plt.ylabel("LM Loss")
+plt.xlabel("Training Step")
+plt.savefig("uni_ckpt_char.png")
+print(tb_log_paths)
