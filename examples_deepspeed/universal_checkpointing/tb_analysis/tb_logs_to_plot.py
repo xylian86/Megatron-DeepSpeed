4f2f81602a66e1bd066e37f3a2cf21edbf15fbf9
Lev Kurilenko <lekurile@microsoft.com>
2024-02-16 22:06:40 +0000

    Move args to separate folder, add sns switch

--- examples_deepspeed/universal_checkpointing/tb_analysis/tb_logs_to_plot.py
+++ examples_deepspeed/universal_checkpointing/tb_analysis/tb_logs_to_plot.py
@@ -9,23 +9,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 # TODO: use sns package (fancy plot)
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
-from argparse import ArgumentParser
 from utils import get_analyzer, find_files_recursive
-#import seaborn as sns
+from arguments import parser
 
-#sns.set()
-
-parser = ArgumentParser()
-parser.add_argument("--tb_dir", required=True, type=str, help="Directory for tensorboard output")
-parser.add_argument("--analyzer", default="universal_checkpointing", type=str, choices=["universal_checkpointing"], help="Specify the analyzer to use")
-parser.add_argument("--tb_event_key", required=False, default="lm-loss-training/lm loss", type=str, help="Optional override of the TensorBoard event key")
-parser.add_argument("--plot_title", required=False, default="Megatron-GPT Universal Checkpointing", type=str, help="Optional override of the plot title")
-parser.add_argument("--plot_x_label", required=False, default="Training Step", type=str, help="Optional override of the plot x-label")
-parser.add_argument("--plot_y_label", required=False, default="LM Loss", type=str, help="Optional override of the plot y-label")
-parser.add_argument("--plot_name", required=False, default="uni_ckpt_char.png", type=str, help="Optional override of the plot file name")
-parser.add_argument("--skip_plot", action='store_true', help="Skip generation of plot file")
-parser.add_argument("--skip_csv", action='store_true', help="Skip generation of csv files")
 args = parser.parse_args()
+
+if args.use_sns:
+    try:
+        import seaborn as sns
+        sns.set()
+    except ImportError as e:
+        print(e)
 
 log_dir = args.tb_dir
 
